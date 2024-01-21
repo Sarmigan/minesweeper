46bffe3c94d7f64e6afab7e80ff3a0f0f2a77492
Sarmigan Sritharan <sarmigan@live.co.uk>
2024-01-21 16:26:29 +0000

    replace with sprites and implement game over

--- src/main.py
+++ src/main.py
@@ -2,35 +2,36 @@ from enum import Enum
 import pygame
 import random
 
-pygame.init()
-
-SPRITE_SIZE = 16
-
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-
-GRID_POS_X, GRID_POS_Y = 10,10
-GRID_LENGTH = 945
-GRID_COLOR = (128, 128, 128)
-TILE_PADDING = 5
+SPRITE_SIZE = 64
 
 COLUMNS, ROWS = 9, 9
 MINE_COUNT = 10
 
-HIDDEN_TILE_COLOR = (220, 220, 220)
-UNHIDDEN_TILE_COLOR = (192, 192, 192)
-TILE_TEXT_FONT = pygame.font.SysFont("arial", 20)
-TILE_TEXT_COLORS = {
-    -1: "red",
-    1: "blue",
-    2: "chartreuse4",
-    3: "firebrick1",
-    4: "blue4",
-    5: "firebrick4",
-    6: "mediumaquamarine",
-    7: "black",
-    8: "seashell3"
-} 
+SCREEN_WIDTH = SPRITE_SIZE * COLUMNS
+SCREEN_HEIGHT = SPRITE_SIZE * ROWS
+
+GRID_POS_X, GRID_POS_Y = 0,0
+GRID_WIDTH, GRID_HEIGHT = SPRITE_SIZE * COLUMNS, SPRITE_SIZE * ROWS
+GRID_COLOR = (128, 128, 128)
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Minesweeper")
+
+SPRITE = {
+    "MINE_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/mine_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "FLAG_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/flag_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "HIDDEN_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/hidden_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "EMPTY_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/empty_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "ONE_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/one_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "TWO_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/two_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "THREE_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/three_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "FOUR_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/four_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "FIVE_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/five_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "SIX_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/six_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "SEVEN_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/seven_tile.png").convert_alpha(), SPRITE_SIZE/16),
+    "EIGHT_TILE": pygame.transform.scale_by(pygame.image.load("./sprites/eight_tile.png").convert_alpha(), SPRITE_SIZE/16)
+}
 
 class TileType(Enum):
     MINE = -1
@@ -53,14 +54,14 @@ class Tile:
         self.is_flagged = False
 
 class Grid():
-    def __init__(self, surface, rows, columns, length, global_x, global_y) -> None:
+    def __init__(self, surface, rows, columns, width, height, global_x, global_y) -> None:
         self.surface = surface
         self.global_x = global_x
         self.global_y = global_y
         self.columns = columns
         self.rows = rows
-        self.length = length
-        self.tile_length = min((length-((self.rows+1)*TILE_PADDING))//self.rows, (length-((self.columns+1)*TILE_PADDING))//self.columns)
+        self.width = width
+        self.height = height
         self.grid = []
 
     def get_neighbor_tiles(self, row, column):
@@ -106,81 +107,84 @@ class Grid():
                 if neighbor.tile_type.value >= 0:
                     neighbor.tile_type = TileType(neighbor.tile_type.value + 1)
 
-    def draw_grid(self, length):
-        pygame.draw.rect(self.surface, GRID_COLOR, (self.global_x, self.global_y, length, length))
-
+    def draw_grid(self):
         for row in range(self.rows):
-            tile_y = self.global_y + TILE_PADDING + row * (self.tile_length + TILE_PADDING)
-
             for column in range(self.columns):
-                tile_x = self.global_x + TILE_PADDING + column * (self.tile_length + TILE_PADDING)
+                self.draw_tile(row, column)
 
-                pygame.draw.rect(self.surface, HIDDEN_TILE_COLOR, (tile_x, tile_y, self.tile_length, self.tile_length))
+    def draw_tile(self, row, column):
+        x = self.global_x + column * SPRITE_SIZE
+        y = self.global_y + row * SPRITE_SIZE
+
+        if not self.grid[row][column].is_hidden:
+            match self.grid[row][column].tile_type:
+                case TileType.MINE:
+                    self.surface.blit(SPRITE["MINE_TILE"], (x, y))
+                case TileType.EMPTY:
+                    self.surface.blit(SPRITE["EMPTY_TILE"], (x, y))
+                case TileType.ONE:
+                    self.surface.blit(SPRITE["ONE_TILE"], (x, y))
+                case TileType.TWO:
+                    self.surface.blit(SPRITE["TWO_TILE"], (x, y))
+                case TileType.THREE:
+                    self.surface.blit(SPRITE["THREE_TILE"], (x, y))
+                case TileType.FOUR:
+                    self.surface.blit(SPRITE["FOUR_TILE"], (x, y))
+                case TileType.FIVE:
+                    self.surface.blit(SPRITE["FIVE_TILE"], (x, y))
+                case TileType.SIX:
+                    self.surface.blit(SPRITE["SIX_TILE"], (x, y))
+                case TileType.SEVEN:
+                    self.surface.blit(SPRITE["SEVEN_TILE"], (x, y))
+                case TileType.EIGHT:
+                    self.surface.blit(SPRITE["EIGHT_TILE"], (x, y))
+        else:
+            if self.grid[row][column].is_flagged:
+                self.surface.blit(SPRITE["FLAG_TILE"], (x, y))
+            else:
+                self.surface.blit(SPRITE["HIDDEN_TILE"], (x, y))
+
+    def reveal_all_tiles(self):
+        for row in range(self.rows):
+            for column in range(self.columns):
+                self.grid[row][column].is_hidden = False
+                self.grid[row][column].is_flagged = False
+                self.draw_tile(row, column)
 
     def click_tile(self, row, column):
         self.grid[row][column].is_hidden = False
-        self.draw_unhidden_tile(row, column)
+        self.draw_tile(row, column)
 
-        if self.grid[row][column].tile_type.value == 0:
+        if self.grid[row][column].tile_type == TileType.MINE:
+            return self.grid[row][column].tile_type
+        elif self.grid[row][column].tile_type == TileType.EMPTY:
             neighbors = self.get_neighbor_tiles(row, column)
             for neighbor in neighbors:
                 if neighbor.is_hidden and not neighbor.is_flagged:
-                    if neighbor.tile_type.value == 0 and neighbor:
+                    if neighbor.tile_type == TileType.EMPTY and neighbor.is_hidden:
                         self.click_tile(neighbor.row, neighbor.column)
                     else:
                         neighbor.is_hidden = False
-                        self.draw_unhidden_tile(neighbor.row, neighbor.column)
+                        self.draw_tile(neighbor.row, neighbor.column)
+        
+        return self.grid[row][column].tile_type
 
     def flag_tile(self, row, column):
-        if self.grid[row][column].is_flagged:
-            self.grid[row][column].is_flagged = not self.grid[row][column].is_flagged
-            self.draw_unflagged_tile(row, column)
-        else:
-            self.grid[row][column].is_flagged = not self.grid[row][column].is_flagged
-            self.draw_flagged_tile(row, column)
-
-    def draw_unhidden_tile(self, row, column):
-        tile_x = self.global_x + TILE_PADDING + column * (self.tile_length + TILE_PADDING)
-        tile_y = self.global_y + TILE_PADDING + row * (self.tile_length + TILE_PADDING)
-
-        pygame.draw.rect(self.surface, UNHIDDEN_TILE_COLOR, (tile_x, tile_y, self.tile_length, self.tile_length))
-        
-        if self.grid[row][column].tile_type.value > 0:
-            tile_text = TILE_TEXT_FONT.render(str(self.grid[row][column].tile_type.value), 1, TILE_TEXT_COLORS[self.grid[row][column].tile_type.value])
-            self.surface.blit(tile_text, (tile_x + (self.tile_length/2 - tile_text.get_width()/2), tile_y + (self.tile_length/2 - tile_text.get_height()/2)))
-        elif self.grid[row][column].tile_type == TileType.MINE:
-            tile_text = TILE_TEXT_FONT.render(str(self.grid[row][column].tile_type.value), 1, TILE_TEXT_COLORS[self.grid[row][column].tile_type.value])
-            self.surface.blit(tile_text, (tile_x + (self.tile_length/2 - tile_text.get_width()/2), tile_y + (self.tile_length/2 - tile_text.get_height()/2)))
-            
-    def draw_flagged_tile(self, row, column):
-        tile_x = self.global_x + TILE_PADDING + column * (self.tile_length + TILE_PADDING)
-        tile_y = self.global_y + TILE_PADDING + row * (self.tile_length + TILE_PADDING)
-
-        pygame.draw.rect(self.surface, HIDDEN_TILE_COLOR, (tile_x, tile_y, self.tile_length, self.tile_length))
-        tile_text = TILE_TEXT_FONT.render("?", 1, "orange")
-        self.surface.blit(tile_text, (tile_x + (self.tile_length/2 - tile_text.get_width()/2), tile_y + (self.tile_length/2 - tile_text.get_height()/2)))
-
-    def draw_unflagged_tile(self, row, column):
-        tile_x = self.global_x + TILE_PADDING + column * (self.tile_length + TILE_PADDING)
-        tile_y = self.global_y + TILE_PADDING + row * (self.tile_length + TILE_PADDING)
-
-        pygame.draw.rect(self.surface, HIDDEN_TILE_COLOR, (tile_x, tile_y, self.tile_length, self.tile_length))
+        self.grid[row][column].is_flagged = not self.grid[row][column].is_flagged
+        self.draw_tile(row, column)
 
 def get_clicked_tile(mouse_x, mouse_y):
-    row = (mouse_y - GRID_POS_Y) // (GRID_LENGTH / ROWS)
-    column = (mouse_x - GRID_POS_X) // (GRID_LENGTH / COLUMNS)
+    row = (mouse_y - GRID_POS_Y) // (GRID_HEIGHT / ROWS)
+    column = (mouse_x - GRID_POS_X) // (GRID_WIDTH / COLUMNS)
 
     return int(row), int(column)
 
 if __name__ == "__main__":
     is_game_over = False
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    grid = Grid(screen, ROWS, COLUMNS, GRID_LENGTH, GRID_POS_X, GRID_POS_Y)
+    grid = Grid(screen, ROWS, COLUMNS, GRID_WIDTH, GRID_HEIGHT, GRID_POS_X, GRID_POS_Y)
     grid.create_grid(MINE_COUNT)
-
-    grid.draw_grid(GRID_LENGTH)
+    grid.draw_grid()
 
     is_running = True
     while is_running:
@@ -193,10 +197,15 @@ if __name__ == "__main__":
                 mouse_x, mouse_y = pygame.mouse.get_pos()
 
                 if mouse_x > GRID_POS_X and mouse_y > GRID_POS_Y:
-                    if mouse_x < GRID_POS_X + GRID_LENGTH and mouse_y < GRID_POS_Y + GRID_LENGTH:
+                    if mouse_x < GRID_POS_X + GRID_WIDTH and mouse_y < GRID_POS_Y + GRID_HEIGHT:
                         if event.button == 1:
                             row, column = get_clicked_tile(mouse_x, mouse_y)
-                            if grid.grid[row][column].is_hidden: grid.click_tile(row, column)
+                            
+                            if grid.grid[row][column].is_hidden: 
+                                clicked_tile = grid.click_tile(row, column)
+                                if clicked_tile == TileType.MINE:
+                                    grid.reveal_all_tiles()
+
                         elif event.button == 3:
                             row, column = get_clicked_tile(mouse_x, mouse_y)
                             if grid.grid[row][column].is_hidden: grid.flag_tile(row, column)
